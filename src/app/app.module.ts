e233e493ba89fe0fb6affa3149e41f430586e779
BRUNO CORTEZAO ARRUDA <brunocortezaoarruda@gmail.com>
2019-06-01 18:23:25 -0300

    Reenvio do projeto final devido erro na tentativa anterior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { SettingsComponent } from './core/settings/settings.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
